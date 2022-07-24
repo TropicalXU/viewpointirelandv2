45b92595e8f732b9d0754dff6794c2ea53d0db6a
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-07-23 20:05:32 -0400

    updated changes

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ const sessionConfig = {
     cookie: {
         //httpOnly - used to protect cookird from client-side use
         httpOnly: true,
-        secure: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
